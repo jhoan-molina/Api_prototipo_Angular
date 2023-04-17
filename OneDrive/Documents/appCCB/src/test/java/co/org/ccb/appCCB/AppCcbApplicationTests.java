3980c839dec0fb9fac68bf96478213634bdc38c2
jhoan-molina <jhoansebastianmolinasierra@gmail.com>
2023-04-17 01:00:13 -0500

    Backend basico Spring Boot

---
+++ OneDrive/Documents/appCCB/src/test/java/co/org/ccb/appCCB/AppCcbApplicationTests.java
@@ -0,0 +1,13 @@
+package co.org.ccb.appCCB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCcbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
