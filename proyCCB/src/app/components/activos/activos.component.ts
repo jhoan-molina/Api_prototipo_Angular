ca2d7e0536e28a90441ac4f919b621a3054a2f74
jhoan-molina <jhoansebastianmolinasierra@gmail.com>
2023-04-18 12:10:28 -0500

    documentacion ts del activo sobre el front

--- proyCCB/src/app/components/activos/activos.component.ts
+++ proyCCB/src/app/components/activos/activos.component.ts
@@ -8,17 +8,24 @@ import { ActivosService } from 'src/app/services/activos.service';
   styleUrls: ['./activos.component.css']
 })
 export class ActivosComponent implements OnInit{
-
+  //se crea objeto de tipo activos
   activo: Activos[] = [];
 
+  //se envia como parametro un objeto de tipo servicio para poder acceder a su contenido
   constructor(private activosService:ActivosService ){
 
   }
+
+  //se llama al metodo de servicios que trae la información, se hace necesario
+  //enviar un array de tipo activos para almacenar lo datos traidos
   ngOnInit(): void {
     this.activosService.getActivos().subscribe(
       response => this.activo = response);
   }
 
+  //llama al metodo de servicio que elimina un dato en especifico,
+  //se hace necesario enviar el id del activo, para saber que elemento
+  // es eliminado
   eliminarActivos(id:number){
     this.activosService.eliminarActivos(id).
     subscribe(response => {
