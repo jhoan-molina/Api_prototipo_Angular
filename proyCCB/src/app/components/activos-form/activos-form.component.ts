27770998a26017463ff7003843ef58f712ff01de
jhoan-molina <jhoansebastianmolinasierra@gmail.com>
2023-04-18 14:05:08 -0500

    documentación del form activos

--- proyCCB/src/app/components/activos-form/activos-form.component.ts
+++ proyCCB/src/app/components/activos-form/activos-form.component.ts
@@ -10,14 +10,17 @@ import { ActivosService } from 'src/app/services/activos.service';
 })
 export class ActivosFormComponent implements OnInit{
 
+  //creamos un objeto de tipo activos
   activo:Activos = new Activos();
 
+  //pasamos como parametro el activoService de tipo activoService y activatedRoute de tipo ActivatedRoute
+  //donde traeremos los servicios que estan alojados alli
   constructor(private activosService:ActivosService, private activatedRoute: ActivatedRoute){
 
   }
 
   ngOnInit(): void {
-    //Actualizar
+    //Actualziar
     this.activatedRoute.params.
     subscribe(params => {let id: number = params['id'];
     if(id){
@@ -26,6 +29,8 @@ export class ActivosFormComponent implements OnInit{
     })
   }
 
+  //Se crean activos a partir de los datos que se crean desde el formulario
+  // se envia un objeto de tipo activo para que el back se encargue
   crearActivos(){
     this.activosService.crearActivos(this.activo).
     subscribe(response => console.log("¡Exito!"));
